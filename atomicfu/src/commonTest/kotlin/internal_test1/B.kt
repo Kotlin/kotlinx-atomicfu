b0ae71d9b20e4d78fe5f3876d9a9c8a9136fff7e
Roman Elizarov <elizarov@gmail.com>
2019-04-23 16:52:56 +0300

    Added more array tests

--- atomicfu/src/commonTest/kotlin/internal_test1/B.kt
+++ atomicfu/src/commonTest/kotlin/internal_test1/B.kt
@@ -12,10 +12,17 @@ class B {
     fun testInternal() {
         val a = A()
         a.internalField.lazySet(true)
+        assertEquals(true, a.internalField.value)
         check(a.internalField.getAndSet(false))
+        assertEquals(false, a.internalField.value)
         check(a.xxx.addAndGet(4) == 9)
+        assertEquals(9, a.xxx.value)
         check(a.yyy.compareAndSet(638753975930025820, 3444))
+        assertEquals(3444, a.yyy.value)
         check(a.intArr[2].compareAndSet(0, 6))
+        assertEquals(6, a.intArr[2].value)
+        check(a.refArr[3].compareAndSet(null, "OK"))
+        assertEquals("OK", a.refArr[3].value)
     }
 }
 
