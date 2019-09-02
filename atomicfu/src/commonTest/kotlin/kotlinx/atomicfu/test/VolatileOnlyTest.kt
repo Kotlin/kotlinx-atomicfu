25195bc901cec11ad6c37e426c8d364966ca74c4
Roman Elizarov <elizarov@gmail.com>
2019-09-02 20:34:20 +0300

    Optimize volatile-only fields in JVM
    Atomic fields that use getValue/setValue are compiled down to the plain
    volatile fields without FieldUpdater/VarHandle attached to them, so
    that all @Volatile field in the source can be replaced with atomics
    without loosing efficiency of the resulting bytecode.
    
    In order to figure this out, method transformer is run in the Phase 2
    on analysis in the special mode, so that data flow can be tracked
    from fields to their method to see what methods are called on each
    fields. Now transformation (Phase 3) is only run on files which this
    Phase 2 had determined to need transformation (so we don't waste time
    reading/reanalyzing class files that don't use atomics).
    
    Also:
    * File processing logic (phasing) is streamlined
    * Array-handling logic is somewhat cleaned up

---
+++ atomicfu/src/commonTest/kotlin/kotlinx/atomicfu/test/VolatileOnlyTest.kt
@@ -0,0 +1,20 @@
+package kotlinx.atomicfu.test
+
+import kotlinx.atomicfu.*
+import kotlin.test.*
+
+/**
+ * Tests atomic fields that work as replacement to volatiles (only getValue/setValue)
+ */
+class VolatileOnlyTest {
+    private val _ref = atomic<String?>(null)
+    private val _int = atomic(0)
+
+    @Test
+    fun testVolatileOnly() {
+        _ref.value = "OK"
+        assertEquals("OK", _ref.value)
+        _int.value = 42
+        assertEquals(42, _int.value)
+    }
+}
