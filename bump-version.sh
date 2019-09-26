211e71c4b38f3ae251579cd42b035c30e620d69f
sokolova <marysok17@gmail.com>
2019-09-26 13:11:15 +0300

    Release checklist added

---
+++ bump-version.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]
+  then
+    echo "Use: ./bump-version old_version new_version"
+    exit
+fi
+
+old_version=$1
+new_version=$2
+
+update_version() {
+    echo "Updating version from '$old_version' to '$new_version' in $1"
+    sed -i.bak s/$old_version/$new_version/g $1
+    rm $1.bak
+}
+
+update_version "README.md"
+update_version "gradle.properties"
+
+# Escape dots, e.g. 1.0.0 -> 1\.0\.0
+escaped_old_version=$(echo $old_version | sed s/[.]/\\\\./g)
+result=$(find ./ -type f \( -iname \*.properties -o -iname \*.md \) | grep -v "\.gradle" | grep -v "build" | xargs -I{} grep -H "$escaped_old_version" {} | grep -v CHANGES.md)
+if [ -z "$result" ];
+then
+    echo "Done"
+else
+    echo "ERROR: Previous version is present in the project: $result"
+    exit -1
+fi
