56ac2fc8e0199a44348bb285159e360befd5e877
Roman Elizarov <elizarov@gmail.com>
2019-11-06 11:43:20 +0300

    kotlinx.metadata version update + SynchronizedObject removal

--- atomicfu/src/jvmMain/kotlin/kotlinx/atomicfu/Synchronized.kt
+++ atomicfu/src/jvmMain/kotlin/kotlinx/atomicfu/Synchronized.kt
@@ -1,6 +1,11 @@
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+
 package kotlinx.atomicfu
+
+import kotlin.internal.InlineOnly
 
 public actual typealias SynchronizedObject = Any
 
+@InlineOnly
 public actual inline fun <T> synchronized(lock: SynchronizedObject, block: () -> T): T =
     kotlin.synchronized(lock, block)
