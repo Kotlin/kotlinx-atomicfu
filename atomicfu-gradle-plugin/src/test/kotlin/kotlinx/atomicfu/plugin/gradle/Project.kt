fb2a88f37d5e0ef5bc5013cfc4abac8bdc30f52d
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-08-25 12:56:38 +0300

    Remove bintray references

--- atomicfu-gradle-plugin/src/test/kotlin/kotlinx/atomicfu/plugin/gradle/Project.kt
+++ atomicfu-gradle-plugin/src/test/kotlin/kotlinx/atomicfu/plugin/gradle/Project.kt
@@ -84,8 +84,6 @@ class Project(val projectDir: File) {
                     jcenter()
                     mavenCentral()
                     maven { url 'https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/dev' }
-                    maven { url 'https://dl.bintray.com/kotlin/kotlin-eap' }
-                    maven { url 'https://dl.bintray.com/kotlin/kotlin-dev' }
                     maven { url 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev' }
                     maven { url "https://oss.sonatype.org/content/repositories/snapshots" }
                 }
