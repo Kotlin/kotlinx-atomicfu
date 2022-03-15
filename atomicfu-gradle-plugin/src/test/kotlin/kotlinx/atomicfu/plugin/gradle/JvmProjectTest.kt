e1e33bd785ae191b4915165987957df51bd0ed01
mvicsokolova <maria.sokolova@jetbrains.com>
2022-03-15 15:46:25 +0300

    Ignore old gradle plugin tests

--- atomicfu-gradle-plugin/src/test/kotlin/kotlinx/atomicfu/plugin/gradle/JvmProjectTest.kt
+++ atomicfu-gradle-plugin/src/test/kotlin/kotlinx/atomicfu/plugin/gradle/JvmProjectTest.kt
@@ -5,10 +5,11 @@
 package kotlinx.atomicfu.plugin.gradle
 
 import org.gradle.testkit.runner.TaskOutcome
-import org.junit.Test
+import org.junit.*
 import java.io.File
 
 class JvmProjectTest : BaseKotlinGradleTest() {
+    @Ignore
     @Test
     fun testKotlinPlugin() =
         project("jvm-simple") {
