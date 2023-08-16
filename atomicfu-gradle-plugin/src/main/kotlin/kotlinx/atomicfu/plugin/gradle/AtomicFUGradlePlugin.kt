2c6b70940060b1fe7c78cccc2f8e21546a48cd4a
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2023-08-16 17:15:04 +0200

    CODEREVIEW Remove transform task for wasm

--- atomicfu-gradle-plugin/src/main/kotlin/kotlinx/atomicfu/plugin/gradle/AtomicFUGradlePlugin.kt
+++ atomicfu-gradle-plugin/src/main/kotlin/kotlinx/atomicfu/plugin/gradle/AtomicFUGradlePlugin.kt
@@ -339,7 +339,7 @@ private fun Project.configureTransformationForTarget(target: KotlinTarget) {
                         }
                 } else null
             }
-            KotlinPlatformType.js, KotlinPlatformType.wasm -> {
+            KotlinPlatformType.js -> {
                 // create transformation task only if transformation is required and JS IR compiler transformation is not enabled
                 if (config.transformJs && !needsJsIrTransformation(target)) {
                     project.registerJsTransformTask(compilation)
@@ -354,6 +354,7 @@ private fun Project.configureTransformationForTarget(target: KotlinTarget) {
                         }
                 } else null
             }
+            KotlinPlatformType.wasm -> null
             else -> error("Unsupported transformation platform '${target.platformType}'")
         }
         if (transformTask != null) {
