4820ad1cd4a73cabeccaab3c888f8b95556974e1
mvicsokolova <maria.sokolova@jetbrains.com>
2023-11-01 16:08:27 +0100

    Unit tests WIP

---
+++ integration-testing/src/functionalTest/kotlin/kotlinx.atomicfu.gradle.plugin.test/unit/ArtifactCheckerTest.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2016-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.atomicfu.gradle.plugin.test.unit
+
+import kotlin.test.Test
+
+class ArtifactCheckerTest {
+    @Test
+    fun 
+}
