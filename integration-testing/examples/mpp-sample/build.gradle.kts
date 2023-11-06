edb13418a6d2f306579325e053e814646a6b77b3
mvicsokolova <maria.sokolova@jetbrains.com>
2023-11-06 19:01:19 +0100

    Add all the native targets to the mpp-sample project config

--- integration-testing/examples/mpp-sample/build.gradle.kts
+++ integration-testing/examples/mpp-sample/build.gradle.kts
@@ -34,18 +34,11 @@ repositories {
 kotlin {
     jvm()
     js()
-
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    macosArm64()
+    macosX64()
+    linuxArm64()
+    linuxX64()
+    mingwX64()
 
     sourceSets {
         commonMain {
