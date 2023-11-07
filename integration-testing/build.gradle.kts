ee10a6fb26093a9a0ba961b7da74817e03a3443d
mvicsokolova <maria.sokolova@jetbrains.com>
2023-11-07 12:11:49 +0100

    Composite build setup: - made integration-testing part of the rootProject - removed duplicated gradle.properties - integration-testing depends on atomicfu-gradle-plugin publication

--- integration-testing/build.gradle.kts
+++ integration-testing/build.gradle.kts
@@ -1,44 +1,12 @@
 import org.jetbrains.kotlin.gradle.utils.NativeCompilerDownloader
 
-buildscript {
-
-    /*
-     * These property group is used to build kotlinx-atomicfu against Kotlin compiler snapshot.
-     * How does it work:
-     * When build_snapshot_train is set to true, kotlin_version property is overridden with kotlin_snapshot_version,
-     * atomicfu_version is overwritten by TeamCity environment (AFU is built with snapshot and published to mavenLocal
-     * as previous step or the snapshot build).
-     * Additionally, mavenLocal and Sonatype snapshots are added to repository list and stress tests are disabled.
-     * DO NOT change the name of these properties without adapting kotlinx.train build chain.
-     */
-    val prop = project.properties["build_snapshot_train"]
-    val build_snapshot_train = prop != null && prop != ""
-    val kotlin_version = project.properties["kotlin_snapshot_version"]
-    if (build_snapshot_train) {
-        if (kotlin_version == null) {
-            throw IllegalArgumentException("'kotlin_snapshot_version' should be defined when building with snapshot compiler")
-        }
-    }
-
-    // Determine if any project dependency is using a snapshot version
-    var using_snapshot_version = build_snapshot_train
-    project.properties.forEach {
-        if (it.key.endsWith("_version") && (it.value as String).endsWith("-SNAPSHOT")) {
-            println("NOTE: USING SNAPSHOT VERSION: ${it.key}=${it.value}")
-            using_snapshot_version = true
-        }
-    }
-
-    if (using_snapshot_version) {
-        repositories {
-            mavenLocal()
-            maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        }
-    }
+repositories {
+    mavenLocal()
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
 plugins {
-    kotlin("jvm") version "${project.properties["kotlin_version"]}" // todo: is there a more consise way to get kotlin_version from gradle.properties?
+    kotlin("jvm")
 }
 
 repositories {
@@ -64,13 +32,15 @@ dependencies {
     implementation(kotlin("script-runtime"))
 }
 
+val atomicfu_snapshot_version = rootProject.properties["version"]
+
 sourceSets {
     create("mavenTest") {
         compileClasspath += files(sourceSets.main.get().output, configurations.testRuntimeClasspath)
         runtimeClasspath += output + compileClasspath
-
+        
         dependencies {
-            implementation("org.jetbrains.kotlinx:atomicfu-jvm:${project.properties["atomicfu_version"]}")
+            implementation("org.jetbrains.kotlinx:atomicfu-jvm:$atomicfu_snapshot_version")
         }
     }
 
@@ -86,16 +56,24 @@ sourceSets {
     }
 }
 
+val checkAfterPublish by tasks.registering {
+    dependsOn(":atomicfu-gradle-plugin:publishToMavenLocal")
+}
+
 val mavenTest by tasks.registering(Test::class) {
     testClassesDirs = sourceSets["mavenTest"].output.classesDirs
     classpath = sourceSets["mavenTest"].runtimeClasspath
+
+    dependsOn(checkAfterPublish)
 }
 
 val functionalTest by tasks.registering(Test::class) {
     testClassesDirs = sourceSets["functionalTest"].output.classesDirs
     classpath = sourceSets["functionalTest"].runtimeClasspath
 
-    systemProperties["atomicfuVersion"] = project.properties["atomicfu_version"]
+    systemProperties["atomicfuVersion"] = atomicfu_snapshot_version
+    
+    dependsOn(checkAfterPublish)
 }
 
 tasks.check { dependsOn(mavenTest, functionalTest) }
@@ -103,7 +81,7 @@ tasks.check { dependsOn(mavenTest, functionalTest) }
 // Setup K/N infrastructure to use klib utility in tests
 // TODO: klib checks are skipped for now because of this problem KT-61143
 val Project.konanHome: String
-get() = project.properties["kotlin.native.home"]?.toString()
+get() = rootProject.properties["kotlin.native.home"]?.toString()
         ?: NativeCompilerDownloader(project).compilerDirectory.absolutePath
 
 val embeddableJar = File(project.konanHome).resolve("konan/lib/kotlin-native-compiler-embeddable.jar")
