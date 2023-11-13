f4253f9ba5eec922764a0843110a8e81d88b689f
mvicsokolova <maria.sokolova@jetbrains.com>
2023-11-13 11:38:11 +0100

    Remove kotlin-stdlib dependency

--- integration-testing/examples/mpp-sample/build.gradle.kts
+++ integration-testing/examples/mpp-sample/build.gradle.kts
@@ -41,7 +41,6 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation(kotlin("stdlib"))
                 implementation(kotlin("test-junit"))
             }
         }
