1161b06b424fe41b69dc72f7be2b0c3b5b1b5707
mvicsokolova <maria.sokolova@jetbrains.com>
2023-11-14 10:49:29 +0100

    Removed @Suppress("ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_MEMBERS_AS_NON_FINAL_EXPECT_CLASSIFIER")

--- atomicfu/src/jsAndWasmSharedMain/kotlin/kotlinx/atomicfu/locks/Synchronized.kt
+++ atomicfu/src/jsAndWasmSharedMain/kotlin/kotlinx/atomicfu/locks/Synchronized.kt
@@ -2,7 +2,6 @@ package kotlinx.atomicfu.locks
 
 import kotlinx.atomicfu.REENTRANT_LOCK
 
-@Suppress("ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_MEMBERS_AS_NON_FINAL_EXPECT_CLASSIFIER")
 public actual typealias SynchronizedObject = Any
 
 @kotlinx.atomicfu.OptionalJsName(REENTRANT_LOCK)
