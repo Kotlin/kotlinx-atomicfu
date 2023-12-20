bab9117580ce5977ddc4ddbd99dc25300eee31ee
Maria Sokolova <maria.sokolova@jetbrains.com>
2023-12-20 13:07:29 +0100

    Added a test that reproduces the plugin order problem.
    Fixes #384

---
+++ integration-testing/examples/plugin-order-bug/src/commonTest/kotlin/AtomicSampleTest.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2016-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import kotlin.test.*
+import examples.mpp_sample.*
+
+class AtomicSampleTest {
+
+    @Test
+    fun testInt() {
+        val a = AtomicSampleClass()
+        a.doWork(1234)
+        assertEquals(1234, a.x)
+        assertEquals(42, a.synchronizedFoo(42))
+    }
+}
---
+++ integration-testing/src/functionalTest/kotlin/kotlinx.atomicfu.gradle.plugin.test/cases/PluginOrderBugTest.kt
@@ -0,0 +1,21 @@
+package kotlinx.atomicfu.gradle.plugin.test.cases
+
+import kotlinx.atomicfu.gradle.plugin.test.framework.runner.*
+import kotlinx.atomicfu.gradle.plugin.test.framework.runner.GradleBuild
+import kotlinx.atomicfu.gradle.plugin.test.framework.runner.cleanAndBuild
+import kotlinx.atomicfu.gradle.plugin.test.framework.runner.createGradleBuildFromSources
+import kotlin.test.*
+
+/**
+ * This test reproduces and tracks the issue #384.
+ */
+class PluginOrderBugTest {
+    private val pluginOrderBugProject: GradleBuild = createGradleBuildFromSources("plugin-order-bug")
+
+    @Test
+    fun testUserProjectBuild() {
+        val buildResult = pluginOrderBugProject.cleanAndBuild()
+        assertFalse(buildResult.isSuccessful)
+        assertTrue(buildResult.output.contains("unresolved reference: kotlinx"), buildResult.output)
+    }
+}
