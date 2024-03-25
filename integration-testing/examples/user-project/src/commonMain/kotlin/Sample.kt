b19c23f8ae072bd7b7b84b00f182dc408ecbf5ab
mvicsokolova <82594708+mvicsokolova@users.noreply.github.com>
2024-03-25 14:03:57 +0100

    ReentrantLock expect class should declare a default constructor (#411)
    Fixes #401

--- integration-testing/examples/user-project/src/commonMain/kotlin/Sample.kt
+++ integration-testing/examples/user-project/src/commonMain/kotlin/Sample.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2017-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 import examples.mpp_sample.*
 import kotlin.test.*
 
@@ -5,5 +9,5 @@ fun doWorld()  {
     val sampleClass = AtomicSampleClass()
     sampleClass.doWork(1234)
     assertEquals(1234, sampleClass.x)
-    assertEquals(42, sampleClass.synchronizedFoo(42))
+    assertEquals(42, sampleClass.synchronizedSetState(42))
 }
