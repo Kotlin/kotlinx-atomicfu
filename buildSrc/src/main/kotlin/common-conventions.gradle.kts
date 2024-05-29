6fec89b7ce16ea2991efea092227f6858660f6c6
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-05-29 12:08:19 +0200

    Moved setting `build_snapshot_train` to extra properties in common-conventions

--- buildSrc/src/main/kotlin/common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/common-conventions.gradle.kts
@@ -1,2 +1,5 @@
 val deployVersion = project.findProperty("DeployVersion")
 if (deployVersion != null) project.version = deployVersion
+
+val buildSnapshotTrainGradleProperty = findProperty("build_snapshot_train")
+extra["build_snapshot_train"] = buildSnapshotTrainGradleProperty != null && buildSnapshotTrainGradleProperty != ""
