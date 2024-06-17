1afc7c84a57f03b7f364a7c780656dbc0de705ce
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-06-17 16:48:24 +0200

    Improve readability of the build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
     alias(libs.plugins.kotlinx.binaryCompatibilityValidator)
 }
 
-val deploy by tasks.creating() {
+val deploy: Task? by tasks.creating {
     dependsOn(getTasksByName("publish", true))
     dependsOn(getTasksByName("publishNpm", true))
 }
 
-// Right now it is used for switching nodejs version which is supports generated wasm bytecode
+// Right now it is used for switching nodejs version which is supports generated wasm bytecode - Remove after updating to Kotlin 2.0
 extensions.findByType(NodeJsRootExtension::class.java)?.let {
     // canary nodejs that supports recent Wasm GC changes
     it.nodeVersion = "21.0.0-v8-canary202309167e82ab1fa2"
