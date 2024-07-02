956393490b90f3549f651ac4e1676897077b4189
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-07-02 18:46:04 +0200

    Remove ignore-engines

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
-
 plugins {
     alias(libs.plugins.kotlinx.binaryCompatibilityValidator)
 }
@@ -8,9 +5,4 @@ plugins {
 val deploy: Task? by tasks.creating {
     dependsOn(getTasksByName("publish", true))
     dependsOn(getTasksByName("publishNpm", true))
-}
-
-// We need to ignore unsupported engines (i.e. canary) for npm
-tasks.withType(KotlinNpmInstallTask::class).configureEach {
-    args.add("--ignore-engines")
 }
