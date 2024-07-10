365341d034807b8bf6f2efd76a0de1b8e791e588
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-07-10 17:01:23 +0200

    Removed duplicated properties

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -4,13 +4,18 @@ dependencyResolutionManagement {
 
     @Suppress("UnstableApiUsage")
     repositories {
+        /*
+        * This property group is used to build kotlinx.atomicfu against Kotlin compiler snapshots.
+        * When build_snapshot_train is set to true, mavenLocal and Sonatype snapshots are added to repository list
+        * (the former is required for AFU and public, the latter is required for compiler snapshots).
+        * DO NOT change the name of these properties without adapting kotlinx.train build chain.
+        */
         val buildSnapshotTrainGradleProperty = providers.gradleProperty("build_snapshot_train")
         if (buildSnapshotTrainGradleProperty.isPresent) {
             maven(url = uri("https://oss.sonatype.org/content/repositories/snapshots"))
         }
 
-        val additionalRepositoryProperty = providers.gradleProperty("community.project.kotlin.repo")
-            .orElse(providers.gradleProperty("kotlin_repo_url"))
+        val additionalRepositoryProperty = providers.gradleProperty("kotlin_repo_url")
         if (additionalRepositoryProperty.isPresent) {
             maven(url = uri(additionalRepositoryProperty.get()))
             logger.info("A custom Kotlin repository ${additionalRepositoryProperty.get()} was added")
@@ -25,26 +30,6 @@ dependencyResolutionManagement {
 
             val kotlinVersion = providers.gradleProperty("kotlin_version").orNull
             if (kotlinVersion != null) {
-                version("kotlin", kotlinVersion)
-            }
-
-            val overridingKotlinVersion =  providers.gradleProperty("community.project.kotlin.version").orNull
-            if (overridingKotlinVersion != null) {
-                logger.info("An overriding Kotlin version of $overridingKotlinVersion was found for buildSrc")
-                version("kotlin", overridingKotlinVersion)
-            }
-
-            /*
-            * This property group is used to build kotlinx.atomicfu against Kotlin compiler snapshots.
-            * When build_snapshot_train is set to true, kotlin_version property is overridden with kotlin_snapshot_version.
-            * Additionally, mavenLocal and Sonatype snapshots are added to repository list
-            * (the former is required for AFU and public, the latter is required for compiler snapshots).
-            * DO NOT change the name of these properties without adapting kotlinx.train build chain.
-            */
-            val buildSnapshotTrainGradleProperty = providers.gradleProperty("build_snapshot_train").orNull
-            if (buildSnapshotTrainGradleProperty != null && buildSnapshotTrainGradleProperty != "") {
-                val kotlinVersion = providers.gradleProperty("kotlin_snapshot_version").orNull
-                    ?: throw IllegalArgumentException("'kotlin_snapshot_version' should be defined when building with a snapshot compiler")
                 version("kotlin", kotlinVersion)
             }
         }
