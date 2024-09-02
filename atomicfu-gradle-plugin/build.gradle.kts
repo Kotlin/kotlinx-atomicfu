604c3122a3b9d1bb5397d18bdb5f6f4fd144b171
Margarita Bobova <margarita.bobova@jetbrains.com>
2024-09-02 19:49:06 +0200

    WA:LV 2.0 for gradle plugin and transformer

--- atomicfu-gradle-plugin/build.gradle.kts
+++ atomicfu-gradle-plugin/build.gradle.kts
@@ -9,6 +9,14 @@ plugins {
     id("gradle-compatibility")
 }
 
+// Gradle plugin must be compiled targeting the same Kotlin version as used by Gradle
+kotlin.sourceSets.configureEach {
+    languageSettings {
+        languageVersion = "2.0"
+        apiVersion = "2.0"
+    }
+}
+
 dependencies {
     implementation(project(":atomicfu-transformer")) {
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
