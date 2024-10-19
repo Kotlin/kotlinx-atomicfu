92c03a5d51628312ef194f3811d661b27240de80
mvicsokolova <82594708+mvicsokolova@users.noreply.github.com>
2024-10-19 19:43:41 +0200

    Fix the failing ModelReport task, which is incompatible with configuration caches. (#481)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,11 @@ val deploy: Task? by tasks.creating {
     dependsOn(getTasksByName("publish", true))
     dependsOn(getTasksByName("publishNpm", true))
 }
+
+// This is a WA for the failure of the :model task during publication:
+// "Task `:model` of type `org.gradle.api.reporting.model.ModelReport`: invocation of 'Task.project' at execution time is unsupported."
+// model task is incompatible with configuration cache, and this WA makes a warning from this error.
+// Also, model tasks are going to be completely removed: https://github.com/gradle/gradle/issues/15023
+tasks.named("model") {
+    notCompatibleWithConfigurationCache("uses Task.project")
+}
