1a777abfbc56cf9d9d2e0f460719aa0554aa9e82
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-11-27 11:21:23 -0500

    KTI-1966: Skip JvmVersionCompatibilityTest (#489)
    JvmVersionCompatibilityTest tests how AFU plugin works
    with the latest JDK version supported by the Kotlin
    compiler.
    The latest JDK version may not be presented on an CI
    agent executing tests and for agents running tests
    from the kotlin-community/dev branch auto provisioning
    is also unavailable.
    This change disables tests that are unstable due to
    above-mentioned reasons.

--- integration-testing/src/functionalTest/kotlin/kotlinx.atomicfu.gradle.plugin.test/cases/JvmVersionCompatibilityTest.kt
+++ integration-testing/src/functionalTest/kotlin/kotlinx.atomicfu.gradle.plugin.test/cases/JvmVersionCompatibilityTest.kt
@@ -3,8 +3,14 @@ package kotlinx.atomicfu.gradle.plugin.test.cases
 import kotlinx.atomicfu.gradle.plugin.test.framework.runner.GradleBuild
 import kotlinx.atomicfu.gradle.plugin.test.framework.runner.cleanAndBuild
 import kotlinx.atomicfu.gradle.plugin.test.framework.runner.createGradleBuildFromSources
+import kotlin.test.Ignore
 import kotlin.test.Test
 
+// This tests may use latest / oldest JDK versions supported by the Kotlin compiler / KGP.
+// These versions may not be available on CI agents executing tests and auto-provisioning
+// not available on agents running tests from the kotlin-community/dev branch.
+// As a result, tests are unstable and have to be disabled.
+@Ignore("KTI-1966")
 class JvmVersionCompatibilityTest {
     private val jvmSample: GradleBuild = createGradleBuildFromSources("jdk-compatibility")
 
