59e1966d83da1ee613471e531bec8494c5188a8b
Maria Sokolova <maria.sokolova@jetbrains.com>
2024-12-03 10:28:00 +0100

    Update ./bump_version.sh script to skip updating the version in the notes "starting from version xxx".

--- bump-version.sh
+++ bump-version.sh
@@ -11,12 +11,18 @@ new_version=$2
 
 update_version() {
     echo "Updating version from '$old_version' to '$new_version' in $1"
-    sed -i.bak s/$old_version/$new_version/g $1
+    sed -i.bak -E "s/$old_version/$new_version/g;s/(from version \`${OLD_VERSION}\`)/\1/g" $1
     rm $1.bak
 }
 
 update_version "README.md"
 update_version "gradle.properties"
+update_version "./integration-testing/examples/mpp-sample/gradle.properties"
+update_version "./integration-testing/examples/plugin-order-bug/gradle.properties"
+update_version "./integration-testing/examples/multi-module-test/gradle.properties"
+update_version "./integration-testing/examples/jdk-compatibility/gradle.properties"
+update_version "./integration-testing/examples/jvm-sample/gradle.properties"
+update_version "./integration-testing/examples/mpp-version-catalog/gradle/libs.versions.toml"
 
 # Escape dots, e.g. 1.0.0 -> 1\.0\.0
 escaped_old_version=$(echo $old_version | sed s/[.]/\\\\./g)
