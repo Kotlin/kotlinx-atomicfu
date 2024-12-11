11f02214d0a2e7b4f5a6bf7749dc75a0f8002b5a
Bob.Brockbernd <bobbrockbernd@gmail.com>
2024-12-11 12:55:48 +0100

    Fix Mutex.withLock: unlock() after exception, allow non-local returns and prevent value capturing in block() by defining contract.
    Fix expected constructor

--- atomicfu/src/commonMain/kotlin/kotlinx/atomicfu/locks/Mutex.kt
+++ atomicfu/src/commonMain/kotlin/kotlinx/atomicfu/locks/Mutex.kt
@@ -1,20 +1,30 @@
 package kotlinx.atomicfu.locks
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 /**
  * Multiplatform mutex.
  * On native based on futex(-like) system calls.
  * On JVM delegates to ReentrantLock.
  */
-expect class Mutex {
+expect class Mutex() {
     fun isLocked(): Boolean
     fun tryLock(): Boolean
     fun lock()
     fun unlock()
 }
-
+@OptIn(ExperimentalContracts::class)
 fun <T> Mutex.withLock(block: () -> T): T {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
     lock()
-    val result = block()
-    unlock()
-    return result
+    return try {
+        block()
+    } finally {
+        unlock()
+    }
 }
+
