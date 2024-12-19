ab7fcba8becf4b06a2b9fbb0d0f17ce4625b6fd3
Bob.Brockbernd <bobbrockbernd@gmail.com>
2024-12-19 11:59:07 +0100

    Comments

--- atomicfu/src/commonMain/kotlin/kotlinx/atomicfu/parking/ParkingDelegator.kt
+++ atomicfu/src/commonMain/kotlin/kotlinx/atomicfu/parking/ParkingDelegator.kt
@@ -1,8 +1,8 @@
 package kotlinx.atomicfu.parking
 
 /**
- * Internal utility that delegates the futex or posix calls in the platform specific way (darwin, linux, windows).
- * On jvm delegates to LockSupport.Park. (The reason we need this on jvm is to verify the mutex with lincheck)
+ * Internal utility that delegates the thread suspending and resuming calls calls in the platform specific way (darwin, linux, windows).
+ * On jvm delegates to LockSupport.Park.
  */
 internal interface ParkingDelegator {
     fun createRef(): Any
