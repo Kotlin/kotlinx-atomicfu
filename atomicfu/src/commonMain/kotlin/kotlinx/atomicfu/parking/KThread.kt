fc003a1616ba7ac36cf0ca64385e31d1012c41d4
Bob.Brockbernd <bobbrockbernd@gmail.com>
2025-01-20 12:29:47 +0100

    Explicitly define return types

--- atomicfu/src/commonMain/kotlin/kotlinx/atomicfu/parking/KThread.kt
+++ atomicfu/src/commonMain/kotlin/kotlinx/atomicfu/parking/KThread.kt
@@ -19,19 +19,19 @@ expect class Parker private constructor() {
          * Parks the current thread, until an [unpark] call is made.
          * Does not park the current thread if [unpark] was called before [park].
          */
-        fun park()
+        fun park(): Unit
 
         /**
          * Parks if [unpark] was not called before, and wakes when either an [unpark] call is made 
          * or [nanos] nanoseconds have passed.
          */
-        fun parkNanos(nanos: Long)
+        fun parkNanos(nanos: Long): Unit
 
         /**
          * Unparks thread [kThread] when parked, otherwise prevents the next [park] call from parking the thread.
          * Does nothing when the thread is already pre-unparked.
          */
-        fun unpark(kThread: KThread)
+        fun unpark(kThread: KThread): Unit
     }
 }
 
--- atomicfu/src/jvmMain/kotlin/kotlinx/atomicfu/parking/KThread.kt
+++ atomicfu/src/jvmMain/kotlin/kotlinx/atomicfu/parking/KThread.kt
@@ -7,11 +7,11 @@ actual class KThread internal actual constructor() {
     }
 }
 
-actual class Parker actual private constructor() {
+actual class Parker private actual constructor() {
     actual companion object {
-        actual fun park() = localKThread.get().parker.park()
-        actual fun parkNanos(nanos: Long) = localKThread.get().parker.parkNanos(nanos)
-        actual fun unpark(kThread: KThread) = kThread.parker.unpark()
+        actual fun park(): Unit = localKThread.get().parker.park()
+        actual fun parkNanos(nanos: Long): Unit = localKThread.get().parker.parkNanos(nanos)
+        actual fun unpark(kThread: KThread): Unit = kThread.parker.unpark()
     }
 }
 
