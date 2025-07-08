2b7b810db6eea81a96672682e594492cc39608aa
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2025-07-08 10:19:09 -0400

    Support a new publication scheme (#551)

--- buildSrc/src/main/kotlin/base-publish-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/base-publish-conventions.gradle.kts
@@ -4,18 +4,8 @@ plugins {
 }
 
 publishing {
-    repositories {
-        maven {
-            // if you change the name, you should change the name of the credential properties on CI as well
-            name = "MavenRepositoryForPublishing"
+    addPublishingRepositoryIfPresent()
 
-            url = mavenRepositoryUri()
-
-            // we use such type of credential because of the configuration cache problems with other types:
-            // https://github.com/gradle/gradle/issues/24040
-            credentials(PasswordCredentials::class)
-        }
-    }
     publications.withType<MavenPublication>().configureEach {
         pom {
             name = project.name
@@ -43,7 +33,6 @@ publishing {
                 url = "https://github.com/Kotlin/kotlinx.atomicfu"
             }
         }
-
 
         signPublicationIfKeyPresent(project, this)
     }
