4f0c809f7758c1257ca330e9392c9a99d825f782
Margarita Bobova <margarita.bobova@jetbrains.com>
2025-07-22 12:15:41 +0200

    LV 2.2 for atomicfu train modules

--- atomicfu-transformer/build.gradle.kts
+++ atomicfu-transformer/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 // Gradle plugin must be compiled targeting the same Kotlin version as used by Gradle
 kotlin.sourceSets.configureEach {
     languageSettings {
-        languageVersion = "2.0"
-        apiVersion = "2.0"
+        languageVersion = "2.2"
+        apiVersion = "2.2"
     }
 }
 
