35427d007e5af69665746666bdc67331d2fd7b6a
Bob.Brockbernd <bobbrockbernd@gmail.com>
2025-07-24 14:22:27 +0200

    Refactor nativemutextest

--- atomicfu/src/concurrentTest/kotlin/kotlinx/atomicfu/locks/NativeMutexTest.kt
+++ atomicfu/src/concurrentTest/kotlin/kotlinx/atomicfu/locks/NativeMutexTest.kt
@@ -9,85 +9,37 @@ private const val N_REPEATS_FAST = 30_000
 
 class NativeMutexTest {
     
-    
     @Test
-    fun testNativeMutexSlow() {
+    fun testNativeMutexSlow() = testNativeMutex(SLEEP_MILLIS, N_REPEATS_SLOW)
+
+    @Test
+    fun testNativeMutexFast() = testNativeMutex(0, N_REPEATS_FAST)
+    
+    private fun testNativeMutex(sleepDuration: Long, iterations: Int) {
         val mutex = NativeMutex()
         val resultList = mutableListOf<String>()
-
-        val futA = Fut {  
-            repeat(N_REPEATS_SLOW) { i ->
-                mutex.lock()
-                resultList.add("a$i")
-                sleepMillis(SLEEP_MILLIS)
-                resultList.add("a$i")
-                mutex.unlock()
-            }
-        }
-
-        val futB = Fut {
-            repeat(N_REPEATS_SLOW) { i ->
-                mutex.lock()
-                resultList.add("b$i")
-                sleepMillis(SLEEP_MILLIS)
-                resultList.add("b$i")
-                mutex.unlock()
-            }
-        }
-
-        repeat(N_REPEATS_SLOW) { i ->
-            mutex.lock()
-            resultList.add("c$i")
-            sleepMillis(SLEEP_MILLIS)
-            resultList.add("c$i")
-            mutex.unlock()
-        }
         
+        fun addResults(id: String) {
+            repeat(iterations) { i ->
+                mutex.lock()
+                resultList.add("$id$i")
+                if (sleepDuration > 0) sleepMillis(sleepDuration)
+                resultList.add("$id$i")
+                mutex.unlock()
+            }
+        }
+
+        val futA = Fut { addResults("a") }
+        val futB = Fut { addResults("b") }
+
+        addResults("c")
+
         futA.waitThrowing()
         futB.waitThrowing()
-        
+
         resultList.filterIndexed { i, _ -> i % 2 == 0 }
             .zip(resultList.filterIndexed { i, _ -> i % 2 == 1 }) { a, b ->
-            assertEquals(a, b)
-        }
-    }
-
-    @Test
-    fun testNativeMutexFast() {
-        val mutex = SynchronousMutex()
-        val resultList = mutableListOf<String>()
-
-        val fut1 = testThread {
-            repeat(N_REPEATS_FAST) { i -> 
-                mutex.lock()
-                resultList.add("a$i")
-                resultList.add("a$i")
-                mutex.unlock()
+                assertEquals(a, b)
             }
-        }
-
-        val fut2 = testThread {
-            repeat(N_REPEATS_FAST) { i -> 
-                mutex.lock()
-                resultList.add("b$i")
-                resultList.add("b$i")
-                mutex.unlock()
-            }
-        }
-
-        repeat(N_REPEATS_FAST) { i ->
-            mutex.lock()
-            resultList.add("c$i")
-            resultList.add("c$i")
-            mutex.unlock()
-        }
-        fut1.join()
-        fut2.join()
-        
-        resultList
-            .filterIndexed { i, _ -> i % 2 == 0 }
-            .zip(resultList.filterIndexed {i, _ -> i % 2 == 1}) { a, b ->
-            assertEquals(a, b)
-        }
     }
 }
