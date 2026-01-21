3bd1fcfa44aa27ac4e1ad3c4d3e6f1cfe94d5085
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-01-21 16:32:27 -0500

    Remove direct dependency on atomicfu-transformer from the Gradle plugin (#569)
    * Remove direct dependency on atomicfu-transformer
    * Use libraries version corresponding to the Kotlin compiler version
    * Cleanup code, address incompatibility w/ future Gradle versions
    
    #556 closed

--- atomicfu-gradle-plugin/build.gradle.kts
+++ atomicfu-gradle-plugin/build.gradle.kts
@@ -10,12 +10,9 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":atomicfu-transformer")) {
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
-    }
-
+    compileOnly(project(":atomicfu-transformer"))
     compileOnly(gradleApi())
-    compileOnly(libs.kotlin.stdlib)
+    compileOnly(libs.kotlin.stdlib.compat)
     compileOnly(libs.kotlin.gradlePlugin)
     // Atomicfu compiler plugin dependency will be loaded to kotlinCompilerPluginClasspath
     // Atomicfu plugin will only be applied if the flag is set kotlinx.atomicfu.enableJsIrTransformation=true
