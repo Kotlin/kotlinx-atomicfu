b3983e29142a6a3af49cf5087b486c3b038e6935
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-03-31 07:49:44 -0400

    Make task lookup lazy

--- buildSrc/src/main/kotlin/JarManifest.kt
+++ buildSrc/src/main/kotlin/JarManifest.kt
@@ -1,9 +1,10 @@
 import org.gradle.api.Project
 import org.gradle.jvm.tasks.Jar
+import org.gradle.kotlin.dsl.withType
 
 internal fun Project.configureImplementationJarManifest(taskName: String) {
-    tasks.matching { it.name == taskName }.configureEach {
-        this as Jar
+    tasks.withType<Jar>().configureEach {
+        if (name != taskName) return@configureEach
         manifest.attributes(
             mapOf(
                 "Implementation-Title" to project.name,
